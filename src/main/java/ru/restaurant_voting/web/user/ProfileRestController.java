b59c54eed6bef1f27ba3ef4d680b79bd01cc914a
Alexandr <Orange179@mail.ru>
2020-07-14 17:07:53 +0300

    add to ProfileRestController method with URI "/register"

--- src/main/java/ru/restaurant_voting/web/user/ProfileRestController.java
+++ src/main/java/ru/restaurant_voting/web/user/ProfileRestController.java
@@ -2,9 +2,12 @@ package ru.restaurant_voting.web.user;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.validation.BindException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import ru.restaurant_voting.model.User;
+
+import java.net.URI;
 
 import static ru.restaurant_voting.web.SecurityUtil.authUserId;
 
@@ -24,9 +27,18 @@ public class ProfileRestController extends AbstractUserController {
         super.delete(authUserId());
     }
 
+    @PostMapping(value = "/register", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public ResponseEntity<User> register(@RequestBody User user) {
+        User created = super.create(user);
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(REST_URL).build().toUri();
+        return ResponseEntity.created(uriOfNewResource).body(created);
+    }
+
     @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void update(@RequestBody User user) throws BindException {
+    public void update(@RequestBody User user) {
         super.update(user, authUserId());
     }
 
